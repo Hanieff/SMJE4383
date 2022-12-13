2596b107bf603631625e59ca61c00f8929143e66
Hanieff <116073026+Hanieff@users.noreply.github.com>
2022-12-12 22:33:18 -0800

    haniff

---
+++ create_array.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+number_1 = np.array([2, 3, 2, 8, 11]) #single dimension array
+
+number_2 = np.array([[1, 2, 3], [5, 10, 11]]) #multi-dimension array
+
+print(number_1)
+print(number_2)
